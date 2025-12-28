f984ada5c949f6a91b0603e87df4dbeecf96f919
LordWhit <161860512+LordWhit@users.noreply.github.com>
2025-12-27 23:58:13 -0600

    Update main.tsx

--- Site-Builder-Pro/client/src/main.tsx
+++ Site-Builder-Pro/client/src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Failed to find the root element. Please make sure there is a <div id=\"root\"></div> in your index.html");
+}
+
+createRoot(rootElement).render(<App />);
